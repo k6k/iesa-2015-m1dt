f17431f660299a24e193f297e11dae2062000b7a
florian <florian7cherel@gmail.com>
2015-03-20 09:52:35 +0100

    Intégration du traitement php  du formulaire sur la meme page que celui-ci

--- 03_form/bad.php
+++ 03_form/bad.php
@@ -5,9 +5,14 @@
 	<title>Form</title>
 </head>
 <body>
-	<form action="post.php" method="post">
+	<form action="#" method="post">
 		<fieldset>
-		<?php $date = date('d/m/Y',time()); ?>
+		<?php 
+			if(isset($_POST['submit'])){
+			echo	((!is_null($_POST['prenom'])) && (!is_null($_POST['nom']))) ? "Vos infos sont : ".$_POST['prenom'].' '.$_POST['nom'] :  '';
+			}
+			$date = date('d/m/Y',time());
+		?>
 			<legend>Entrez vos informations, nous sommes le <?php echo $date ?></legend>
 			<label for="nom">Nom</label><br>
 			<input type="text" name='nom'><br><br>
@@ -15,7 +20,7 @@
 			<label for="prenom">Prenom</label><br>
 			<input type="text" name='prenom'><br><br>
 
-			<input type="submit" value="Envoyer"><br>
+			<input type="submit" value="Envoyer" name="submit"><br>
 		</fieldset>
 	</form>
 </body>
